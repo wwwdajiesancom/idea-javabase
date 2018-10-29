b06232fb7fe93b60e30e6be5ad4989f20a837bf7
wwwdajiesancom <805240128@qq.com>
2018-10-29 14:23:57 +0800

    '增加了.gitignore'

---
+++ javabase/src/test/java/com/loujie/www/java20181029_01/Java20181029_01.java
@@ -0,0 +1,17 @@
+package com.loujie.www.java20181029_01;
+
+import org.junit.Test;
+
+/**
+ * @name loujie
+ * @date 2018/10/29
+ */
+public class Java20181029_01 {
+
+    @Test
+    public void test_01() {
+        System.out.println("-----------------------------");
+    }
+
+
+}
