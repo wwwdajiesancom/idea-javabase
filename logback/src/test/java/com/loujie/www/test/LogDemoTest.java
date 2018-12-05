9a13d4e6b22ae2357de618fc63dc90b636e168b9
wwwdajiesancom <805240128@qq.com>
2018-12-05 09:58:18 +0800

    'logback日志'

--- logback/src/test/java/com/loujie/www/test/LogDemoTest.java
+++ logback/src/test/java/com/loujie/www/test/LogDemoTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 /**
  * @name loujie
  * @date 2018/11/21
@@ -12,25 +14,29 @@ public class LogDemoTest {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+
     @Test
     public void soutTest() throws InterruptedException {
-        logger.debug("-----------soutTest----------");
-        logger.info("-----------soutTest----------");
-        logger.warn("-----------soutTest----------");
-        logger.error("-----------soutTest----------");
-        new Thread(new Runnable() {
-            public void run() {
-                abc();
-            }
-        }).start();
-        this.abc();
-        Thread.sleep(3000);
+        while (true) {
+            logger.debug("-----------soutTest----------");
+            logger.info("-----------soutTest----------");
+            logger.warn("-----------soutTest----------");
+            logger.error("-----------soutTest----------");
+            new Thread(new Runnable() {
+                public void run() {
+                    abc();
+                }
+            }).start();
+            this.abc();
+            Thread.sleep(350);
+        }
     }
 
-    public void abc(){
+    public void abc() {
         logger.debug("------abc------debug");
         logger.info("------abc------info");
         logger.warn("------abc------warn");
         logger.error("------abc------error");
     }
+
 }
